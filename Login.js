26a2dcfabb23ddcca1d179a2e46abbccb5bebe73
John Nelson <nel21048@byui.edu>
2022-06-23 13:46:14 -0600

    not working code

--- Login.js
+++ Login.js
@@ -1,10 +1,40 @@
 import {useState} from "react";
-import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Text } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Text, View } from "react-native";
+
+const sendText = async (phoneNumber) => {
+
+  // using fetch do a POST to https://dev.stedi.me/twofactorlogin/9497933305
+  console.log("Phone Number: ",phoneNumber);
+  const loginResponse = await fetch('https://dev.stedi.me/twofactorlogin/'+phoneNumber,{
+    method: 'POST',
+    headers: {
+      'content-type':'application/text'
+    }
+  });
+  const loginResponseText = await loginResponse.text(); //converts the promise to a string by using await
+  console.log('Login Response',loginResponseText);//print the response
+}
+
+const getToken = async({phoneNumber,otp}) =>{//THIS CODE IS NOT COMPLETE
+  console.log('PhoneNumber',phoneNumber);
+  console.log('OTP',oneTimePassword);
+  const loginResponse = await fetch('https://dev.stedi.me/twofactorlogin',{
+    method: 'POST',
+    headers: {
+      'content-type':'application/text'
+    },
+    body:{
+      phoneNumber,
+      oneTimePassword
+    }
+  });
+  const token = await loginResponse.text();
+  console.log(token);
+}
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [oneTimePassword, setOneTimePassword] = useState(null);
-  const onPress = () => setCount(prevCount => prevCount + 1);
 
   return (
     <SafeAreaView style={styles.margin}>
